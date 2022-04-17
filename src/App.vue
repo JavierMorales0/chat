38876cb2ed5b43ea77e2efd21250e4af45e0ca09
JavierMorales0 <javiermoralesmelara@gmail.com>
2022-04-16 19:40:15 -0600

    Created the comunication with the socket server

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,49 @@
-<template >
+<template>
   <div>
-    
+    <input type="text" v-model="nickname" v-if="!connected" />
+    <button v-on:click.prevent="connect(nickname)" v-if="!connected">
+      Conectar
+    </button>
+    <button v-on:click.prevent="disconnect(nickname)" v-if="connected">
+      Desconectar
+    </button>
   </div>
 </template>
 <script>
+import io from "socket.io-client";
 export default {
-  
-}
+  data() {
+    return {
+      socket: null,
+      connected: false,
+      nickname: null,
+    };
+  },
+  methods: {
+    connect(nickname) {
+      if (!nickname) {
+        alert("Por favor, escriba un nickname");
+        return;
+      }
+      this.socket = io(
+        "https://jalex-chat.herokuapp.com/chat?nickname=" + nickname
+      );
+      this.connected = true;
+      this.socket.on("connect", () => {
+        console.log("Connected");
+      });
+      this.socket.on("message", (data) => {
+        console.log(data);
+      });
+      this.socket.on("disconnect", () => {
+        console.log("Disconnected");
+      });
+    },
+    disconnect() {
+      this.socket.disconnect();
+      this.connected = false;
+    },
+  },
+};
 </script>
-<style lang="">
-  
-</style>
+<style lang=""></style>
