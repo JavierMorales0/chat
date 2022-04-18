a29e02539a0a475f4da463e6c85eca9b790ee624
JavierMorales0 <javiermoralesmelara@gmail.com>
2022-04-17 20:53:30 -0600

    Iniciada la interfaz

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,81 @@
 <template>
-  <div>
-    <input type="text" v-model="nickname" v-if="!connected" />
-    <button v-on:click.prevent="connect(nickname)" v-if="!connected">
-      Conectar
-    </button>
-    <button v-on:click.prevent="disconnect(nickname)" v-if="connected">
-      Desconectar
-    </button>
-  </div>
+  <header class="py-1 py-md-3 px-1 px-md-3 text-center">
+    <p class="_title">Chat</p>
+  </header>
+  <main class="d-flex justify-around align-items-center">
+    <div class="px-3 py-2 w-25 _border-radius" v-show="!isConnected">
+      <el-input
+        v-model="nickname"
+        placeholder="Ingrese su nombre de usuario"
+        class="my-2 w-100"
+      /><el-button
+        class="d-block mx-auto my-2"
+        v-on:click.prevent="connect(nickname)"
+        >Ir al chat</el-button
+      >
+    </div>
+    <div v-show="!isConnected">c</div>
+    <div v-show="isConnected" class="_bg-primary w-100 p-0">
+      <div class="px-2 px-md-4 my-0">
+        <p class="text-center py-2">
+          Conectado como <span class="bold">{{ nickname }}</span>
+        </p>
+      </div>
+      <section class="bg-white px-1 px-md-3">
+        <div
+          class="_border-radius"
+          v-for="(item, index) in messages"
+          :key="index"
+        >
+          <p class="d-block w-100 my-0">
+            <span class="bold"> {{ item.nickname }}</span
+            >: <span>{{ item.message }}</span>
+          </p>
+        </div>
+      </section>
+    </div>
+  </main>
 </template>
 <script>
 import io from "socket.io-client";
+import { ElMessage } from "element-plus";
 export default {
   data() {
     return {
       socket: null,
-      connected: false,
+      isConnected: false,
       nickname: null,
+      messages: [],
     };
+  },
+  mounted() {
+    // Traer del localstorage el nickname
+    this.nickname = localStorage.getItem("nickname");
+    if (!this.nickname) {
+      ElMessage.error("No hay sesión iniciada");
+    } else {
+      this.connect(this.nickname);
+    }
   },
   methods: {
     connect(nickname) {
-      if (!nickname) {
-        alert("Por favor, escriba un nickname");
+      if (nickname == "" || nickname.length < 3) {
+        ElMessage.warning(
+          "El nombre de usuario debe tener al menos 3 caracteres"
+        );
         return;
       }
       this.socket = io(
         "https://jalex-chat.herokuapp.com/chat?nickname=" + nickname
       );
-      this.connected = true;
+      this.isConnected = true;
+      // Setear el nickname en el localstorage
+      localStorage.setItem("nickname", nickname);
       this.socket.on("connect", () => {
         console.log("Connected");
       });
       this.socket.on("message", (data) => {
-        console.log(data);
+        this.messages.push(data);
       });
       this.socket.on("disconnect", () => {
         console.log("Disconnected");
@@ -41,9 +83,46 @@ export default {
     },
     disconnect() {
       this.socket.disconnect();
-      this.connected = false;
+      this.isConnected = false;
+      localStorage.removeItem("nickname");
     },
   },
 };
 </script>
-<style lang=""></style>
+<style lang="scss">
+* {
+  font-family: "PT Sans", sans-serif;
+}
+html,
+body,
+#app {
+  height: 100%;
+}
+
+:root {
+  --primary: #61c57a;
+}
+._title {
+  font-size: 1.3rem;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-shadow: 0px 0px 10px var(--primary);
+  text-transform: lowercase;
+}
+
+.h-100 {
+  height: 100% !important;
+}
+
+._bg-primary {
+  background-color: var(--primary) !important;
+}
+
+.bold {
+  font-weight: bold;
+}
+
+._border-radius {
+  border-radius: 10px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "bootstrap/dist/css/bootstrap.css";
+import ElementPlus from "element-plus";
+import es from "element-plus/es/locale/lang/es";
+import "@/assets/index.scss";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(ElementPlus, {
+    locale: es,
+  })
+  .use(store)
+  .use(router)
+  .mount("#app");
