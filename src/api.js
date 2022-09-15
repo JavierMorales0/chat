22d9e8b04bb779088b45f2d211867fba4e099cab
Javier Morales <javiermoralesmelara@gmail.com>
2022-09-15 16:57:36 -0600

    ADDED FEATURES: last-users API
    Created and finished the endpoint to get last connected users

--- src/api.js
+++ src/api.js
@@ -17,13 +17,45 @@ router.get("/online-users", async (req, res) => {
 });
 
 router.get("/last-users", async (req, res) => {
-  const response = await HistoryModel.find({ status: "offline" })
-    .populate({
-      path: "user",
-      select: "username email avatar",
-    })
-    .sort([["disconnectionDate", -1]])
-    .limit(10);
+  const response = await HistoryModel.aggregate([
+    {
+      // Group by user Id
+      $group: {
+        _id: "$user",
+        // Get how many times did the user get connected
+        times: { $sum: 1 },
+        lastConnection: { $last: "$connectionDate" },
+      },
+    },
+    {
+      // Sort by connectionDate fron close to far
+      $sort: { lastConnection: -1 },
+    },
+    {
+      // Limit 5 registers
+      $limit: 5,
+    },
+    {
+      // Populating user data
+      $lookup: {
+        from: "users",
+        localField: "_id",
+        foreignField: "_id",
+        as: "user",
+      },
+    },
+    {
+      // Merge the array to one single object
+      $replaceRoot: {
+        newRoot: {
+          $mergeObjects: [{ $arrayElemAt: ["$user", 0] }, "$$ROOT"],
+        },
+      },
+    },
+
+    // Setting which parameters i want on the response
+    { $project: { _id: 0, status: 0, user: 0, __v: 0 } },
+  ]);
   res.json(response);
 });
 export default router;
